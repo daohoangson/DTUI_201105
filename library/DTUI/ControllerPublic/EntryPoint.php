274c728a64a5be7532b2c3b17d93d653ffcbca07
Dao Hoang Son <daohoangson@gmail.com>
2011-05-08 09:47:04 +0700

    demo

--- library/DTUI/ControllerPublic/EntryPoint.php
+++ library/DTUI/ControllerPublic/EntryPoint.php
@@ -12,12 +12,37 @@ class DTUI_ControllerPublic_EntryPoint extends XenForo_ControllerPublic_Abstract
 		$table = $this->_getTableOrError($tableId);
 		
 		var_dump($tableId);exit;
+		
+		return $this->responseView('DTUI_ViewPublic_EntryPoint_Tables');
+	}
+	
+	public function actionTable() {
+		$tableId = $this->_input->filterSingle('data', XenForo_Input::UINT);
+		$table = $this->_getTableOrError($tableId);
+		
+		$viewParams = array(
+			'table' => $table,
+		);
+		
+		return $this->responseView('DTUI_ViewPublic_EntryPoint_Table', '', $viewParams);
+	}
+	
+	public function actionNewOrder() {
+		
 	}
 	
 	protected function _getTableOrError($tableId) {
 		$tableModel = $this->_getTableModel();
-		$info = $tableModel->getTableById($tableid);
-		// TODO: from here
+		$info = $tableModel->getTableById($tableId);
+		if (empty($info)) {
+			throw new XenForo_Exception(new XenForo_Phrase('dtui_table_not_found'), true);
+		}
+		
+		return $info;
+	}
+	
+	protected function _getTableModel() {
+		return $this->getModelFromCache('DTUI_Model_Table');
 	}
 	
 	protected function _preDispatchFirst($action) {
