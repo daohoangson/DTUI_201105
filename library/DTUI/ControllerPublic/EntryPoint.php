cac0648dc813b23ae40a780cb30b16cc897631a2
unknown <HOANG MANH@.(none)>
2011-05-08 13:38:52 +0700

    addNewOrder And Task

--- library/DTUI/ControllerPublic/EntryPoint.php
+++ library/DTUI/ControllerPublic/EntryPoint.php
@@ -26,10 +26,45 @@ class DTUI_ControllerPublic_EntryPoint extends XenForo_ControllerPublic_Abstract
 		
 		return $this->responseView('DTUI_ViewPublic_EntryPoint_Table', '', $viewParams);
 	}
-	
+	/**
+	 * date : 08/05/2011
+	 */
 	public function actionNewOrder() {
+		$input = $this->_input->filter(array(
+		'table_id' => XenForo_Input::UINT,
+		'item_ids' => array(XenForo_Input::UINT, 'array' => true)
+		));
 		
+		
+		$orderDw = XenForo_DataWriter::create('DTUI_DataWriter_Order');// new order
+		$orderDw->set('table_id', $input['table_id']);
+		$orderDw->set('order_date',XenForo_Application::$time);
+		$orderDw->save();// storage new order into Oder table in database;
+		
+		$order = $orderDw->getMergedData();// get data is saved ;
+		// create items for a order 
+		foreach ($input['item_ids'] as $itemId) {
+			$orderItemDw = XenForo_DataWriter::create('DTUI_DataWriter_OrderItem');// a item in a order
+			$orderItemDw->set('order_id', $order['order_id']);
+			$orderItemDw->set('trigger_user_id',XenForo_Visitor::getUserId());
+			$orderItemDw->set('target_user_id',0);
+			$orderItemDw->set('item_id',$itemId);
+			$orderItemDw->set('order_item_date', XenForo_Application::$time);
+			$orderItemDw->set('status','waiting');
+			
+			$tmp = $orderItemDw->save();// storage new order_item into OrderItem table in Database
+			
+		}
+		die('Ok');
 	}
+	public function actionTasks(){// return list order_items of a user 
+		$userIdtmp = XenForo_Visitor::getUserId();
+		$conditions = array('userId' => $userIdtmp);
+		$order_items = $this->getModelFromCache('DTUI_Model_OrderItem')->getAllOrderItem($conditions);
+		$viewParams = array('items' => $order_items);
+		return $this -> responseView('DTUI_ViewPublic_EntryPoint_Tasks','',$viewParams);
+	}
+	
 	
 	protected function _getTableOrError($tableId) {
 		$tableModel = $this->_getTableModel();
---
+++ library/DTUI/ViewPublic/EntryPoint/Tasks.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Created on May 8, 2011
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ class DTUI_ViewPublic_EntryPoint_Tasks extends XenForo_ViewPublic_Base {
+	public function renderHtml() {
+		die('html');
+	}
+	
+	public function renderJson() {
+		$items = $this->_params['items'];
+		
+		return array(
+			'items' => $items,
+		);
+	}
+ }
+?>
