17e00272bb6dcd294375c39c30206057e90589fc
Dao Hoang Son <daohoangson@gmail.com>
2011-05-15 13:21:54 +0700

    dtui-entry-point/tables for ManhHX

--- library/DTUI/ControllerPublic/EntryPoint.php
+++ library/DTUI/ControllerPublic/EntryPoint.php
@@ -7,27 +7,9 @@ class DTUI_ControllerPublic_EntryPoint extends DTUI_ControllerPublic_EntryPointQ
 	
 		return $this->responseView('DTUI_ViewPublic_EntryPoint_Index', '', $viewParams);
     }
-
-    public function actionTables() {
-		$tableId = $this->_input->filterSingle('data', XenForo_Input::UINT);
-	
-		$table = $this->_getTableOrError($tableId);
-	
-		var_dump($tableId);
-		exit;
-	
-		return $this->responseView('DTUI_ViewPublic_EntryPoint_Tables');
-    }
-
-    public function actionTable() {
-		$tableId = $this->_input->filterSingle('data', XenForo_Input::UINT);
-		$table = $this->_getTableOrError($tableId);
-	
-		$viewParams = array(
-		    'table' => $table,
-		);
-	
-		return $this->responseView('DTUI_ViewPublic_EntryPoint_Table', '', $viewParams);
+    
+    public function actionUserInfo() {
+    	return $this->responseView('DTUI_ViewPublic_EntryPoint_UserInfo', '', $viewParams);
     }
 
 	protected function _getTableOrError($tableId) {
